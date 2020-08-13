42b95e8d763457faebc6eb737f5ec9be864bbca0
tanakorn <tanakorn@central.tech>
2020-08-13 11:33:51 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Modules/Home/Home';
+import UseRef from './Modules/UseRef/UseRef';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const routes = [
+        {
+            path: '/home',
+            name: 'Home',
+        },
+        {
+            path: '/use-ref',
+            name: 'UseRef Recat Hooks',
+        },
+    ];
+
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        {routes.map(route => {
+                            return (
+                                <li>
+                                    <Link to={route.path}>{route.name}</Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route exact path="/home" component={Home} />
+                    <Route exact path="/use-ref" component={UseRef} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Modules/UseRef/UseRef.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UseRef() {
+    return <h1>UseRef</h1>;
+}
+
+export default UseRef;
